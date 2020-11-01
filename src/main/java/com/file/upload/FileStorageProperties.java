f09554104786ee9b7a9a91c17bf36e0febaa788b
Archana J Shenoy <archana31011995@gmail.com>
2020-11-01 10:43:38 +0530

    deploy

---
+++ src/main/java/com/file/upload/FileStorageProperties.java
@@ -0,0 +1,19 @@
+package com.file.upload;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@ConfigurationProperties(prefix="file")
+public class FileStorageProperties {
+	private String uploadDir;
+
+	public String getUploadDir() {
+		return uploadDir;
+	}
+
+	public void setUploadDir(String uploadDir) {
+		this.uploadDir = uploadDir;
+	}
+	
+	
+}
